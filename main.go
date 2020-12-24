d4540f33d4d77463e7c8445b8d7452d77319fab4
Simon Eason <sie.eason@gmail.com>
2020-12-24 08:54:32 +0800

    all tokens added to array in main program

--- main.go
+++ main.go
@@ -6,10 +6,16 @@ import (
 )
 
 func main() {
+	var tokenList []tokens.Token
+
 	lines := tokens.ReadFileLines("file.cnl")
 	containerTokenList := tokens.MakeContainerTokens(lines)
 	for i := 0; i < len(containerTokenList); i++ {
 		token := tokens.MakeLineTokens(containerTokenList[i])
+		tokenList = append(tokenList, token)
+
 		fmt.Println(token)
 	}
+
+	fmt.Println(tokenList)
 }
