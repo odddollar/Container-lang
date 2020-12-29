22a3896c8b74fe6f3ba2bad1f43cc2d24e30779e
Simon Eason <sie.eason@gmail.com>
2020-12-29 12:56:57 +0800

    added determining whether to run function or variable stuff in parser

--- supporting/parser/parser.go
+++ supporting/parser/parser.go
@@ -2,9 +2,14 @@ package parser
 
 import (
 	"../structs"
+	"fmt"
 )
 
 func Parse(token structs.Token, tokenList []structs.Token) {
 	// decide if function is being called or variable is being operated on
-
+	if token.FunctionToken.Function == "" { // run variable stuff
+		fmt.Println("variable")
+	} else if token.VarToken.Variable == "" { // run function stuff
+		fmt.Println("function")
+	}
 }
