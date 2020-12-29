2521facc78c5b0e19f8bbb43f21678e39de5d451
Simon Eason <sie.eason@gmail.com>
2020-12-29 12:45:05 +0800

    added parser phase to main function and parser skeleton function

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"./supporting/parser"
 	"./supporting/structs"
 	"./supporting/tokens"
 	"fmt"
 )
 
 func main() {
+	// create empty array of tokens
 	var tokenList []structs.Token
 
+	// read lines of file to array
 	lines := tokens.ReadFileLines("file.cnl")
+	// split lines up into container tokens
 	containerTokenList := tokens.MakeContainerTokens(lines)
+	// iterate through container tokens, splitting into normal tokens
 	for i := 0; i < len(containerTokenList); i++ {
 		token := tokens.MakeLineTokens(containerTokenList[i])
 		tokenList = append(tokenList, token)
@@ -19,4 +24,10 @@ func main() {
 	}
 
 	fmt.Println(tokenList)
+
+	// run parser
+	// iterate through array of tokens
+	for i := 0; i < len(tokenList); i++ {
+		parser.Parse(tokenList[i], tokenList)
+	}
 }
