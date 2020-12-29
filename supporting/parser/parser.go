0bde9fadb97bdd9bf04472912d13209c328977ca
Simon Eason <sie.eason@gmail.com>
2020-12-29 12:45:57 +0800

    added basic parser skeleton function

---
+++ supporting/parser/parser.go
@@ -0,0 +1,10 @@
+package parser
+
+import (
+	"../structs"
+)
+
+func Parse(token structs.Token, tokenList []structs.Token) {
+	// decide if function is being called or variable is being operated on
+
+}
