3e0e0442d9115d018b673c02df237c03984a2ec0
Simon Eason <sie.eason@gmail.com>
2020-12-31 16:38:36 +0800

    added variable struct and function to check if variable exists

---
+++ supporting/parser/checkVarExists.go
@@ -0,0 +1,18 @@
+package parser
+
+import (
+	"../structs"
+)
+
+func checkVarExists(varName string, varList []structs.Variable) bool {
+	// iterate through list of variables
+	for i := 0; i < len(varList); i++ {
+		// if variable is found return true
+		if varList[i].Name == varName {
+			return true
+		}
+	}
+
+	// if variable isn't found return false
+	return false
+}
---
+++ supporting/structs/variables.go
@@ -0,0 +1,6 @@
+package structs
+
+type Variable struct {
+	Name string
+	Value string
+}
