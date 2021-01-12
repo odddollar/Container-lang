a57eea351ce88bb9d70bc81577bebab363797330
Simon Eason <sie.eason@gmail.com>
2021-01-12 14:46:43 +0800

    checking if parsed argument-to-print, erroring if not variable or numerical expression

--- supporting/parser/functions/print.go
+++ supporting/parser/functions/print.go
@@ -5,28 +5,51 @@ import (
 	"fmt"
 	"github.com/Knetic/govaluate"
 	"log"
+	"regexp"
 	"strconv"
 )
 
 func Print(text string, currentContainerID int, varList []structs.Variable) {
-	// create govaluate expression from parsed in print parameters
-	expression, err := govaluate.NewEvaluableExpression(text)
-	if err != nil {
-		log.Fatal("Runtime Error: Container ID " + strconv.Itoa(currentContainerID) + ": Unable to create expression from '" + text + "'")
-	}
+	// create regex to search for only letters
+	findLetters, _ := regexp.Compile("[a-zA-Z]*")
 
-	// create dictionary of variables and their values
-	params := make(map[string]interface{}, 64)
+	// get locations of any letters in the parsed text-to-print
+	letterLocations := findLetters.FindStringIndex(text)
+
+	// check if text is a variable and if letters are present to prevent printing of raw strings but allow printing of expressions and variables
+	if (letterLocations[0] != 0 || letterLocations[1] != 0) && checkIfVar(text, varList) == false {
+		log.Fatal("Runtime error: Container ID " + strconv.Itoa(currentContainerID) + ": Invalid numerical expression/variable '" + text + "'")
+	} else {
+		// create govaluate expression from parsed in print parameters
+		expression, err := govaluate.NewEvaluableExpression(text)
+		if err != nil {
+			log.Fatal("Runtime Error: Container ID " + strconv.Itoa(currentContainerID) + ": Unable to create expression from '" + text + "'")
+		}
+
+		// create dictionary of variables and their values
+		params := make(map[string]interface{}, 64)
+		for i := 0; i < len(varList); i++ {
+			// add variable as number to dictionary
+			params[varList[i].Name], _ = strconv.ParseFloat(varList[i].Value, 64)
+		}
+
+		// evaluate expression
+		result, _ := expression.Evaluate(params)
+
+		// print result
+		fmt.Println(result)
+	}
+}
+
+func checkIfVar(varName string, varList []structs.Variable) bool {
+	// iterate through list of variables
 	for i := 0; i < len(varList); i++ {
-		// add variable as number to dictionary
-		params[varList[i].Name], _ = strconv.ParseFloat(varList[i].Value, 64)
+		// if variable is found return true
+		if varList[i].Name == varName {
+			return true
+		}
 	}
 
-	// evaluate expression
-	result, _ := expression.Evaluate(params)
-
-	// print result
-	fmt.Println(result)
-
-	//log.Fatal("Runtime error: Container ID " + strconv.Itoa(currentContainerID) + ": Invalid number/variable '" + text + "'")
+	// if variable isn't found return false
+	return false
 }
