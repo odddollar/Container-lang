677c24ff29202310cede9b4170549acd757e94f2
Simon Eason <sie.eason@gmail.com>
2021-01-18 15:15:31 +0800

    added ability to execute blocks

--- supporting/parser/parser.go
+++ supporting/parser/parser.go
@@ -68,8 +68,17 @@ func Parse(token structs.Token, tokenList []structs.Token) {
 			// return token after finding it in list
 			executedToken := getContainerById(idToExecute, tokenList, token.Id)
 
-			// recursively call parser function with new token
-			Parse(executedToken, tokenList)
+			// check if executing block or normal container
+			if len(executedToken.Block) == 0 {
+				// recursively call parser function with new token
+				Parse(executedToken, tokenList)
+			} else {
+				// iterate through tokens in block
+				for i := 0; i < len(executedToken.Block); i++ {
+					// recursively call parser function with current token and list of tokens in block
+					Parse(executedToken.Block[i], executedToken.Block)
+				}
+			}
 		}
 	}
 }
